0527e0ad70a163dad117ac3e614bf04f4173d400
DAW2 <DAW2@ieszayas.org>
2025-12-16 11:09:39 +0100

    id de pokemon en equipo unico

--- backend/models/Team.js
+++ backend/models/Team.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const TeamSchema = new Schema({
   id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
-  pokemons: { type: [Number], required: true }, // Array of Pokemon IDs
+  pokemons: { type: [Number], required: true, unique: true }, // Array of Pokemon IDs
 });
 
 module.exports = mongoose.model("Team", TeamSchema);
