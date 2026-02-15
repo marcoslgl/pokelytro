7535c6e42302e24817c361c98e515a75673abd01
Marcos <126660576+pitumola@users.noreply.github.com>
2026-02-15 10:51:32 +0100

    arreglada la url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: typeof window !== 'undefined' && window.location.origin.includes('vercel')
-    ? 'https://https://pokelytro-backend.vercel.app/'
+    ? 'https://pokelytro-backend.vercel.app'
     : 'http://localhost:3000',
   api: {
     users: '/api/users',
