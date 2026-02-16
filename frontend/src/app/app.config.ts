364f7498941cd1727fed059b29f5d84d1be27b5c
ahmad-azhari <ahmadk18elazhari@gmail.com>
2026-02-16 20:58:26 +0100

    Retoques finales

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor'; // Asegúrate de que la ruta sea correcta
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideHttpClient(
       withFetch(),
-      // Añado interceptor de autenticación
       withInterceptors([authInterceptor])
     ),
   ]
